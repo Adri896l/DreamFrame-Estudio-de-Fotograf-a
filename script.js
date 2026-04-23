c35567dde29607251d9655fbbfcafc46b638745a
Adriana <acc9908@gmail.com>
2026-04-23 00:06:29 -0600

    Actualización con Bootstrap

--- script.js
+++
@@ -1,8 +0,0 @@
- const form = document.getElementById("formulario");
-  const alerta = document.getElementById("alerta");
-
-  form.addEventListener("submit", function(e){
-    e.preventDefault(); // evita recarga
-    alerta.classList.remove("d-none"); // muestra alerta
-    form.reset(); // limpia formulario
-  });
